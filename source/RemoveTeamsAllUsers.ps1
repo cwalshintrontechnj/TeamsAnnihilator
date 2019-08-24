341d8fceb011ded4e1bdeff66dbb95609c22db7a
cwalshintrontechnj <54478990+cwalshintrontechnj@users.noreply.github.com>
2019-08-24 12:13:49 -0400

    Update RemoveTeamsAllUsers.ps1

--- source/RemoveTeamsAllUsers.ps1
+++ source/RemoveTeamsAllUsers.ps1
@@ -1,3 +1,5 @@
+#Thanks to Russ2964 on Spiceworks for this snipit.
+
 if (test-path C:\Users\*\AppData\Local\Microsoft\Teams\current\Teams.exe) 
 {kill -name teams -force; 
 (Get-ItemProperty C:\Users\*\AppData\Local\Microsoft\Teams\Current).PSParentPath | foreach-object {Start-Process $_\Update.exe -ArgumentList "--uninstall /s" -PassThru -Wait} 
